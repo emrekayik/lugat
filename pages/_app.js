7db808e1020be1b4a70706d5a2f44e28a0059c4c
Emre Kayık <54414075+emrekayik@users.noreply.github.com>
2023-08-02 23:29:12 +0000

    use pwa

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,6 @@
-import { useEffect } from 'react'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/service-worker.js')
-        .then((registration) => console.log('scope is: ', registration.scope));
-    }
-  }, []);
 
   return <Component {...pageProps} />
 }
